914e4f55f6c2ca172210a607186fe9a70699bd53
Amulya Reddy Konda <amulyareddyk97@gmail.com>
2022-01-24 10:44:41 +0530

    #3 Updated Flyway script

--- src/main/resources/db/migration/V8__Create_Books_Order.sql
+++ src/main/resources/db/migration/V8__Create_Books_Order.sql
@@ -3,11 +3,7 @@ CREATE TABLE IF NOT EXISTS public.books_order
     book_id bigint NOT NULL,
     order_id bigint NOT NULL,
     CONSTRAINT fk92bolhxv3i3imr8omx2xlqlil FOREIGN KEY (order_id)
-        REFERENCES public.books (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
+        REFERENCES public.books (id),
     CONSTRAINT fkg77fhl098601qaymww10hhd2r FOREIGN KEY (book_id)
-        REFERENCES public.orders (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
+        REFERENCES public.orders (id)
 )
