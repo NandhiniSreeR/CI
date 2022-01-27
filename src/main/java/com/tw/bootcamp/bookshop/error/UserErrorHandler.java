b9d3814ebfd0797c90fe423bf7bed79ca7a4a484
NandhiniSree <nandhini.sree@thoughtworks.com>
2022-01-27 11:52:02 +0530

    +Tharun Prasath | Added exception classes

--- src/main/java/com/tw/bootcamp/bookshop/error/UserErrorHandler.java
+++ src/main/java/com/tw/bootcamp/bookshop/error/UserErrorHandler.java
@@ -1,9 +1,11 @@
 package com.tw.bootcamp.bookshop.error;
 
+import com.tw.bootcamp.bookshop.book.error.RequiredBookQuantityNotAvailableException;
 import com.tw.bootcamp.bookshop.payment.exception.InvalidCreditCardDetailsException;
 import com.tw.bootcamp.bookshop.user.InvalidEmailException;
 import com.tw.bootcamp.bookshop.user.InvalidEmailPatternException;
 import com.tw.bootcamp.bookshop.user.InvalidPasswordException;
+import com.tw.bootcamp.bookshop.user.order.error.OrderQuantityCannotBeZeroException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,7 +13,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class UserErrorHandler {
-    @ExceptionHandler({ InvalidEmailException.class, EmailDoesNotExistException.class, InvalidCreditCardDetailsException.class, InvalidEmailPatternException.class, InvalidPasswordException.class})
+    @ExceptionHandler({ InvalidEmailException.class, EmailDoesNotExistException.class, InvalidCreditCardDetailsException.class, InvalidEmailPatternException.class, InvalidPasswordException.class, OrderQuantityCannotBeZeroException.class, RequiredBookQuantityNotAvailableException.class})
     public ResponseEntity<ErrorResponse> handleCreateUserError(Exception ex) {
         ErrorResponse apiError = new ErrorResponse(HttpStatus.UNPROCESSABLE_ENTITY, ex.getMessage());
         return new ResponseEntity<>(apiError, apiError.getStatus());
