fb482ef3edee8740ec45313d894efd1f531513f0
Devesh Mankar <devesh.mankar@gmail.com>
2022-01-28 15:42:11 +0530

    +Sunil | Add JPA Methods for filtering orders

--- src/main/java/com/tw/bootcamp/bookshop/user/order/OrderRepository.java
+++ src/main/java/com/tw/bootcamp/bookshop/user/order/OrderRepository.java
@@ -3,6 +3,14 @@ package com.tw.bootcamp.bookshop.user.order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findAllByOrderDateAfter(Date startDate);
+
+    List<Order> findAllByOrderDateBefore(Date endDate);
+
+    List<Order> findAllByOrderDateBetween(Date startDate, Date endDate);
 }
